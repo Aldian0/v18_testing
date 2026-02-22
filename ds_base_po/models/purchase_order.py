f87dbdf2a6f3efa7586612a1c516f3bd9ae234d9
rizkidesu <s4y4pu1an6@gmail.com>
2026-02-22 13:00:24 +0700

    Fix: apply .gitignore

--- ds_base_po/models/purchase_order.py
+++ ds_base_po/models/purchase_order.py
@@ -5,35 +5,53 @@ class PurchaseOrder(models.Model):
 
     # Ekstensi field state tanpa merusak alur standar Odoo
     state = fields.Selection(selection_add=[
-        ('to_approve_manager', 'Approve Manager'),
-        ('to_approve_dept', 'Approve Dept Head'),
-        ('to_approve_cfo', 'Approve CFO'),
-        ('rejected', 'Rejected')
-    ], default='draft', tracking=True)
+        # ('to_approve_manager', 'Approve Manager'),
+        # ('to_approve_dept', 'Approve Dept Head'),
+        # ('to_approve_cfo', 'Approve CFO'),
+        ('to_confim', 'Draft'),
+        # ('confirm', 'C'),
+        ('rejected', 'Rejected'),
+        ('to_accept', 'To Accept'),
+        ('accept', 'Accept'),
+
+
+    ], default='to_confim', tracking=True)
+
+
+    def action_to_accept(self):
+        # for rec in self:
+        self.state = 'to_accept'
+    def action_accept(self):
+        # for rec in self:
+        self.state = 'draft'
+
+    # def action_rejected(self):
+    #     # for rec in self:
+    #     self.state = 'rejected'
 
     rejection_reason = fields.Text(string="Alasan Penolakan", tracking=True)
 
-    def action_submit_for_approval(self):
-        """
-        Mengevaluasi nilai PO dan menentukan tahap persetujuan berikutnya.
-        """
-        for rec in self:
-            amount = rec.amount_total
+    # def action_submit_for_approval(self):
+    #     """
+    #     Mengevaluasi nilai PO dan menentukan tahap persetujuan berikutnya.
+    #     """
+    #     for rec in self:
+    #         amount = rec.amount_total
             
-            # Logika Filter berdasarkan Nominal PO
-            if amount < 5000000:
-                rec.state = 'to_approve_manager'
-                next_role = "Manager"
-            elif 5000000 <= amount <= 20000000:
-                rec.state = 'to_approve_dept'
-                next_role = "Kepala Departemen"
-            else:
-                rec.state = 'to_approve_cfo'
-                next_role = "CFO"
+    #         # Logika Filter berdasarkan Nominal PO
+    #         if amount < 5000000:
+    #             rec.state = 'to_approve_manager'
+    #             next_role = "Manager"
+    #         elif 5000000 <= amount <= 20000000:
+    #             rec.state = 'to_approve_dept'
+    #             next_role = "Kepala Departemen"
+    #         else:
+    #             rec.state = 'to_approve_cfo'
+    #             next_role = "CFO"
             
-            # Mencatat di chatter & memicu notifikasi email ke follower/grup terkait
-            msg = _("PO sebesar IDR {:,.2f} diajukan. Menunggu persetujuan dari: {}").format(amount, next_role)
-            rec.message_post(body=msg, subtype_xmlid='mail.mt_comment')
+    #         # Mencatat di chatter & memicu notifikasi email ke follower/grup terkait
+    #         msg = _("PO sebesar IDR {:,.2f} diajukan. Menunggu persetujuan dari: {}").format(amount, next_role)
+    #         rec.message_post(body=msg, subtype_xmlid='mail.mt_comment')
 
     def action_approve_custom(self):
         """

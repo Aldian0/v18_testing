29f36c33922da451364e4741ae726cb0845a8964
Aldian0 <alsanahh409@gmail.com>
2026-02-24 10:05:24 +0700

    add revision button

--- ds_base_po/models/purchase_order.py
+++ ds_base_po/models/purchase_order.py
@@ -1,13 +1,12 @@
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    # Ekstensi field state tanpa merusak alur standar Odoo
+    # Ekstensi field state 
     state = fields.Selection(selection_add=[
-        # ('to_approve_manager', 'Approve Manager'),
-        # ('to_approve_dept', 'Approve Dept Head'),
-        # ('to_approve_cfo', 'Approve CFO'),
+        
         ('to_confim', 'Draft'),
         # ('confirm', 'C'),
         ('rejected', 'Rejected'),
@@ -17,41 +16,32 @@ class PurchaseOrder(models.Model):
 
     ], default='to_confim', tracking=True)
 
+    rejection_reason = fields.Text(string="Alasan Penolakan", tracking=True)
 
     def action_to_accept(self):
         # for rec in self:
         self.state = 'to_accept'
     def action_accept(self):
         # for rec in self:
-        self.state = 'draft'
+        self.state = 'draft' 
 
-    # def action_rejected(self):
-    #     # for rec in self:
-    #     self.state = 'rejected'
+    revision_no = fields.Integer(string="Revision", default=0, copy=False)
+    base_name = fields.Char(string="Base Name", copy=False)
 
-    rejection_reason = fields.Text(string="Alasan Penolakan", tracking=True)
+    def action_create_revision(self):
+        for rec in self:
 
-    # def action_submit_for_approval(self):
-    #     """
-    #     Mengevaluasi nilai PO dan menentukan tahap persetujuan berikutnya.
-    #     """
-    #     for rec in self:
-    #         amount = rec.amount_total
-            
-    #         # Logika Filter berdasarkan Nominal PO
-    #         if amount < 5000000:
-    #             rec.state = 'to_approve_manager'
-    #             next_role = "Manager"
-    #         elif 5000000 <= amount <= 20000000:
-    #             rec.state = 'to_approve_dept'
-    #             next_role = "Kepala Departemen"
-    #         else:
-    #             rec.state = 'to_approve_cfo'
-    #             next_role = "CFO"
-            
-    #         # Mencatat di chatter & memicu notifikasi email ke follower/grup terkait
-    #         msg = _("PO sebesar IDR {:,.2f} diajukan. Menunggu persetujuan dari: {}").format(amount, next_role)
-    #         rec.message_post(body=msg, subtype_xmlid='mail.mt_comment')
+            # Simpan nama awal pertama kali
+            if not rec.base_name:
+                rec.base_name = rec.name
+
+            # Tambah revision
+            rec.revision_no += 1
+
+            # Generate nama baru
+            rec.name = f"{rec.base_name}-R{rec.revision_no}"
+    
+    
 
     def action_approve_custom(self):
         """
